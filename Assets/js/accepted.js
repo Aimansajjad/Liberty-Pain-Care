8a5fbe139212b98e5623defa1bfd50f07aeeaa24
Aiman Sajjad <aimansajjad200@gmail.com>
2025-08-06 01:16:20 +0500

    Initial commit - website added

---
+++ Assets/js/accepted.js
@@ -0,0 +1,27 @@
+function clickfaq1() {
+    var x = document.getElementById("faq1div");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+
+  function clickfaq2() {
+    var x = document.getElementById("faq1div2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function clickfaq3() {
+    var x = document.getElementById("faq1div3");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
